6e28c0f30531d3d4a25d8b6faf1f7fdc92cfe84e
Alex Thelander <60659566+weirdabyte@users.noreply.github.com>
2021-11-29 12:41:01 +0100

    Add files via upload

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,62 +1,65 @@
-import React from 'react';
-import {PropTypes} from 'prop-types';
-import { fetchPopularRepos } from '../utils/api'
-import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
+/** @format */
 
-function LanguagesNav ({ selected, onUpdateLanguage}) {
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { fetchPopularRepos } from '../utils/api';
+import {
+  FaUser,
+  FaStar,
+  FaCodeBranch,
+  FaExclamationTriangle,
+} from 'react-icons/fa';
+import Card from './Card';
+import Loading from './Loading';
+import Tooltip from './Tooltip';
 
-    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+function LangaugesNav({ selected, onUpdateLanguage }) {
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
-    return (
-      <ul className='flex-center'>
-        {languages.map((language) => (
-          <li key={language}>
-            <button
-              className='btn-clear-nav-link'
-              style={
-                language === selected
-                  ? { color: 'rgb(187, 46, 31' }
-                  : null
-              }
-              onClick={() => onUpdateLanguage(language)}
+  return (
+    <ul className='flex-center'>
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className='btn-clear nav-link'
+            style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+LangaugesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired,
+};
+
+function ReposGrid({ repos }) {
+  return (
+    <ul className='grid space-around'>
+      {repos.map((repo, index) => {
+        const { name, owner, html_url, stargazers_count, forks, open_issues } =
+          repo;
+        const { login, avatar_url } = owner;
+
+        return (
+          <li key={html_url}>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
             >
-              {language}
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-
-  LanguagesNav.propTypes = {
-      selected: PropTypes.string.isRequired,
-      onUpdateLanguage: PropTypes.func.isRequired
-  }
-
-  function ReposGrid ({ repos }) {
-    return (
-      <ul className='grid space-around'>
-        {repos.map((repo, index) => {
-          const {name, owner, html_url, stargazers_count, forks, open_issues} = repo
-          const { login, avatar_url } = owner
-
-          return (
-            <li key={html_url} className='repo bg-light'>
-              <h4 className='header-lg center-text'>#{index + 1}</h4>
-              <img
-                className='avatar'
-                src={avatar_url}
-                alt={`avatar for ${login}`}
-              />
-              <h2 className='center-text'>
-                <a className='link' href={html_url}>
-                  {login}
-                </a>
-              </h2>
               <ul className='card-list'>
                 <li>
-                  <FaUser color='rgb(255, 191, 116)' size={22} />
-                  <a href={`https://github.com/${login}`}>{login}</a>
+                  <Tooltip text='Github username'>
+                    <FaUser color='rgb(255, 191, 116)' size={22} />
+                    <a href={`https://github.com/${login}`}>{login}</a>
+                  </Tooltip>
                 </li>
                 <li>
                   <FaStar color='rgb(255, 215, 0)' size={22} />
@@ -71,42 +74,35 @@ function LanguagesNav ({ selected, onUpdateLanguage}) {
                   {open_issues.toLocaleString()} open
                 </li>
               </ul>
-            </li>
-          );
-        })}
-      </ul>
-    )
-  }
-ReposGrid.propTypes = {
-  repos: PropTypes.array.isRequired
+            </Card>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
+ReposGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+};
+
 export default class Popular extends React.Component {
-  constructor(props) {
-    super(props)
+  state = {
+    selectedLanguage: 'All',
+    repos: {},
+    error: null,
+  };
 
-    this.state = {
-      selectedLanguage: 'All',
-      repos: {},
-      error: null
-    }
-
-    this.updateLanguage = this.updateLanguage.bind(this);
-    this.isLoading = this.isLoading.bind(this);
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
   }
-
-  componentDidMount () {
-      this.updateLanguage(this.state.selectedLanguage)
-  }
-
-  updateLanguage(selectedLanguage) {
+  updateLanguage = (selectedLanguage) => {
     this.setState({
       selectedLanguage,
       error: null,
     });
 
     if (!this.state.repos[selectedLanguage]) {
-
       fetchPopularRepos(selectedLanguage)
         .then((data) => {
           this.setState(({ repos }) => ({
@@ -116,36 +112,38 @@ export default class Popular extends React.Component {
             },
           }));
         })
-        .catch(() => {
+        .catch((error) => {
           console.warn('Error fetching repos: ', error);
 
           this.setState({
-            error: 'There was an error fetching the repositories.',
+            error: `There was an error fetching the repositories.`,
           });
         });
     }
-  } 
-  isLoading() {
-const { selectedLanguage, repos, error } = this.state
+  };
+  isLoading = () => {
+    const { selectedLanguage, repos, error } = this.state;
 
-return !repos[selectedLanguage] && error === null;
-  }
-
+    return !repos[selectedLanguage] && error === null;
+  };
   render() {
     const { selectedLanguage, repos, error } = this.state;
-    
+
     return (
-        <React.Fragment>
-            <LanguagesNav
-            selected={selectedLanguage}
-            onUpdateLanguage={this.updateLanguage}
-            />
+      <React.Fragment>
+        <LangaugesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
 
-            {this.isLoading() && <p>LOADING</p>}
+        {this.isLoading() && <Loading text='Fetching Repos' />}
 
-            {error && <p>{error}</p>}
+        {error && <p className='center-text error'>{error}</p>}
 
-            {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}   </React.Fragment>
-    )
+        {repos[selectedLanguage] && (
+          <ReposGrid repos={repos[selectedLanguage]} />
+        )}
+      </React.Fragment>
+    );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+/** @format */
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './app/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            {test: /\.(js)$/, use: 'babel-loader'},
-            {test: /\.css$/, use: [ 'style-loader', 'css-loader']}
-        ]
-    },
-    mode: 'development',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'app/index.html'
-        })
-    ]
-}
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
